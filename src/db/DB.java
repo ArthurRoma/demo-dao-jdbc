6c2c11805398adcc315353459e369e3d366f2e99
Arthur Roma <lopestui88@gmail.com>
2025-01-13 22:09:41 -0300

    DaoFacotry

--- src/db/DB.java
+++ src/db/DB.java
@@ -12,15 +12,15 @@ import java.util.Properties;
 public class DB {
 
 	private static Connection conn = null;
-
+	
 	public static Connection getConnection() {
-		if(conn == null) {
+		if (conn == null) {
 			try {
-			Properties props = loadProperties();
-			String url = props.getProperty("dburl");
-			conn = DriverManager.getConnection(url, props);
-		}
-			catch(SQLException e) {
+				Properties props = loadProperties();
+				String url = props.getProperty("dburl");
+				conn = DriverManager.getConnection(url, props);
+			}
+			catch (SQLException e) {
 				throw new DbException(e.getMessage());
 			}
 		}
@@ -28,7 +28,7 @@ public class DB {
 	}
 	
 	public static void closeConnection() {
-		if(conn != null) {
+		if (conn != null) {
 			try {
 				conn.close();
 			} catch (SQLException e) {
@@ -38,36 +38,33 @@ public class DB {
 	}
 	
 	private static Properties loadProperties() {
-	try(FileInputStream fs = new FileInputStream("db.properties")){
-		Properties props = new Properties();
-		props.load(fs);
-		return props;
+		try (FileInputStream fs = new FileInputStream("db.properties")) {
+			Properties props = new Properties();
+			props.load(fs);
+			return props;
+		}
+		catch (IOException e) {
+			throw new DbException(e.getMessage());
+		}
 	}
-	catch(IOException e) {
-		throw new DbException(e.getMessage());
-	}
-  }
 	
 	public static void closeStatement(Statement st) {
 		if (st != null) {
-			try{
-			st.close();
+			try {
+				st.close();
+			} catch (SQLException e) {
+				throw new DbException(e.getMessage());
 			}
-		 catch(SQLException e) {
-			 throw new DbException(e.getMessage());
 		}
 	}
-}
+
 	public static void closeResultSet(ResultSet rs) {
 		if (rs != null) {
-			try{
-			rs.close();
+			try {
+				rs.close();
+			} catch (SQLException e) {
+				throw new DbException(e.getMessage());
 			}
-		 catch(SQLException e) {
-			 throw new DbException(e.getMessage());
 		}
 	}
-}
-	
-	
 }
