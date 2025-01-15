d58da931d7b40efddb361fa402e607e593b45135
Arthur Roma <lopestui88@gmail.com>
2025-01-15 20:59:52 -0300

    update function

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,5 +1,6 @@
 package application;
 
+import java.util.Date;
 import java.util.List;
 
 import model.dao.DaoFactory;
@@ -30,6 +31,19 @@ public class Program {
 		for (Seller  obj : list) {
 			System.out.println(obj);
 		}
+		
+		System.out.println("\n=== TEST 4: seller insert ====");
+		Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.0, department);
+		sellerDao.insert(newSeller);
+		System.out.println("Inserted! New uid = " + newSeller.getId());
+		
+		System.out.println("\n=== TEST : seller updated ====");
+		seller = sellerDao.findById(1);
+		seller.setName("Martha Waine");
+		sellerDao.update(seller);
+		System.out.println("Update competed");
+		
+		
 	}
 
 }
--- src/model/dao/DaoFactory.java
+++ src/model/dao/DaoFactory.java
@@ -2,9 +2,14 @@ package model.dao;
 
 import db.DB;
 import model.dao.impl.SellerDaoJDBC;
+import model.dao.impl.DepartmentDaoJDBC;
 
 public class DaoFactory {
 	public static SellerDao createSellerDao() {
 		return new SellerDaoJDBC(DB.getConnection());
+	}
+	
+	public static DepartmentDao createDepartentDao() {
+		return new DepartmentDaoJDBC(DB.getConnection());
 	}
 }
